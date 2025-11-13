30019d5ac2be82ce0c8bb45a19b24e64c42ab1b4
Urtzi <urtzi.maruri.48440@ikasle.egibide.org>
2025-11-13 11:43:09 +0100

    RamaUrtzi2

--- Programacion/Proyecto5/src/Main.java
+++ Programacion/Proyecto5/src/Main.java
@@ -1,80 +1,41 @@
-import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
-
-
-public static void equipos() {
-
+public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    boolean valido;
-    do {
-        valido = true;
-        try {
-            System.out.println("Ingrese el nombre del equipo ");
-            String nombreEquipo = sc.nextLine();
-            Pattern pattern = Pattern.compile("[A-Za-z]+$");
-            Matcher mat = pattern.matcher(nombreEquipo);
-            if(mat.matches()){
-                valido = true;
-            } else {
-                valido = false;
-                System.out.println("Cadena incorrecta");
-            }
-            if (nombreEquipo.isEmpty()) {
-                throw new Exception("El nombre no puede estar vacío.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error en el equipo");
-            valido = false;
-        }
-    }while(!valido);
-    do {
-        try {
-            System.out.println("Ingrese la fecha de fundacion del equipo ");
-            String fechaInput = sc.nextLine();
-            LocalDate fechaFundacion = validarFecha(fechaInput);
-        } catch (Exception e) {
-            System.out.println("Error en el fecha de fundacion del equipo");
-            valido = false;
-        }
-    }while(!valido);
-    do {
-        try {
-            System.out.println("Ingrese los jugadores del equipo ");
-            String jugador = sc.nextLine();
-            Pattern pattern = Pattern.compile("^(?:[1-9]|1[0-9]|2[0-4])$");
-            Matcher mat = pattern.matcher(jugador);
-            if(mat.matches()){
-                valido = true;
-            } else {
-                valido = false;
-                System.out.println("Cadena incorrecta");
-            }
-            if (jugador.isEmpty()) {
-                throw new Exception("El nombre no puede estar vacío.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error en los jugadores del equipo");
-            valido = false;
-        }
-    }while(!valido);
-}
-public static LocalDate validarFecha(String fechaInput) throws Exception {
-    try {
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate fecha = LocalDate.parse(fechaInput, formato);
+    int respuesta = 0;
 
+    do {
+        try {
+            System.out.println("\n========= MENU =========\n" +
+                    "1) Inscribir Equipos \n" +
+                    "2) Inscribir Jugadores\n" +
+                    "3) Salir");
 
-        if (fecha.isAfter(LocalDate.now())) {
-            throw new Exception("La fecha no puede ser futura.");
+            respuesta = sc.nextInt();
+            sc.nextLine();
+
+            switch (respuesta) {
+                case 1:
+                    equipos();
+                    break;
+                case 2:
+                    jugadores();
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error: Debe introducir un número válido.");
+            sc.nextLine();
         }
-        return fecha;
-    } catch (DateTimeParseException e) {
-        throw new Exception("Formato de fecha incorrecto. Use dd/MM/yyyy");
+
+    } while (respuesta != 3);
+
+    sc.close();
+
     }
 }
 
